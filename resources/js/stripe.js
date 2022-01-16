9e7d51ff0ae83a10481b91680e6861a968bd0bb4
DevYadav-RaRa <dev.yadav@rara.delivery>
2022-01-16 16:26:55 +0530

    project running

--- resources/js/stripe.js
+++ resources/js/stripe.js
@@ -3,7 +3,7 @@
  import { CardWidget } from './CardWidget'
 
 export async function initStripe() {
-    const stripe = await loadStripe('pk_test_51Hf6vbDfWW6uHRy7b6Upoa4bWhGYmk2ElEN4AiFeqozzlccU6OpAoD9d1oIar3qg9i8ASiOs9ly4rFJQOTPM5MMk00GN7COA2N');
+    const stripe = await loadStripe('pk_test_51KHYp9SCzNK55GscRwIh4mCC776soHFetJUeRjBxlmTCgYCHmO2LWcvoRBiJkUz9abzVH1QaFXQJD07p8zasJDZX002rNzRa8h');
     let card = null;
     // function mountWidget() {
     //         const elements = stripe.elements()
